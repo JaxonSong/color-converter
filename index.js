5330b9c629c6882384057d3249828bf0ad5b95b5
宋志贤 <Songzhixian@lunaon.com>
2019-08-09 09:18:57 +0800

    fix converter function

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var rgb2hsb = require('./lib/color_converter.js')
-var hsb2Rgb = require('./lib/color_converter.js')
+var rgb2Hsb = require('./lib/color_converter.js').rgb2Hsb
+var hsb2Rgb = require('./lib/color_converter.js').hsb2Rgb
 
 if (typeof window !== 'undefined') {
-  window.blending = rgb2hsb
-  window.lut = hsb2Rgb
+  window.rgb2Hsb = rgb2Hsb
+  window.hsb2Rgb = hsb2Rgb
 }
 
 module.exports = {
-  rgb2hsb: rgb2hsb,
+  rgb2Hsb: rgb2Hsb,
   hsb2Rgb: hsb2Rgb
 }
